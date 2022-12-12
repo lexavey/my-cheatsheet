93a1c9eed608495e3169d881bdd46ca2f03a0dab
Lex <48092847+lexavey@users.noreply.github.com>
2022-12-13 04:59:09 +0700

    fix

--- Install/git.sh
+++ Install/git.sh
@@ -4,8 +4,8 @@ tar zxf git.tar.gz
 mv git-*/ git
 cd git
 make configure
-./configure --prefix="$INSTALL_DIR/local" --with-curl --with-expat
-# ./configure --prefix="$INSTALL_DIR/local"
+#./configure --prefix="$INSTALL_DIR/local" --with-curl --with-expat
+./configure --prefix="$INSTALL_DIR/local"
 # Make flags from https://public-inbox.org/git/CAP8UFD2gKTourXUdB_9_FZ3AEECTDc1Fx1NFKzeaTZDWHC3jxA@mail.gmail.com/
 make NO_GETTEXT=Nope NO_TCLTK=Nope
 make install NO_GETTEXT=Nope NO_TCLTK=Nope
