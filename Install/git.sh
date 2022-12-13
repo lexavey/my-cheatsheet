4acde99793d6de2a9590d4a8f0bc42ac8e824960
Lex <48092847+lexavey@users.noreply.github.com>
2022-12-13 08:35:40 +0700

    fix

--- Install/git.sh
+++ Install/git.sh
@@ -3,14 +3,14 @@ wget -O git.tar.gz https://github.com/git/git/archive/refs/tags/v2.39.0.tar.gz #
 tar zxf git.tar.gz
 mv git-*/ git
 cd git
-NO_EXPAT=YesPlease NO_OPENSSL=YesPlease NO_CURL=YesPlease make configure
+make configure NO_EXPAT=Yes NO_OPENSSL=Yes NO_CURL=Yes 
 # https://chromium.googlesource.com/external/github.com/git/git/+/HEAD/INSTALL
 # https://gist.github.com/taicki/323800/71b227197e6a94f87ff450bfd6c7fb3ced6b2e49
 #./configure --prefix="$INSTALL_DIR/local" --with-curl --with-expat
 ./configure --prefix="$INSTALL_DIR/local"
 # Make flags from https://public-inbox.org/git/CAP8UFD2gKTourXUdB_9_FZ3AEECTDc1Fx1NFKzeaTZDWHC3jxA@mail.gmail.com/
-NO_EXPAT=YesPlease NO_OPENSSL=YesPlease NO_CURL=YesPlease make NO_GETTEXT=YesPlease NO_TCLTK=Nope 
-NO_EXPAT=YesPlease NO_OPENSSL=YesPlease NO_CURL=YesPlease make NO_GETTEXT=YesPlease NO_TCLTK=Nope
+make NO_GETTEXT=YES NO_TCLTK=YES NO_EXPAT=Yes NO_OPENSSL=Yes NO_CURL=Yes 
+make NO_GETTEXT=YES NO_TCLTK=YES NO_EXPAT=Yes NO_OPENSSL=Yes NO_CURL=Yes 
 $INSTALL_DIR/local/bin/git -v
 
 
