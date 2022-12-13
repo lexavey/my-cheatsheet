0fff059f8fa5ceedc423472bf4683eff4912884e
Lex <48092847+lexavey@users.noreply.github.com>
2022-12-13 19:10:38 +0700

    x

--- Install/open-vscode-server.sh
+++ Install/open-vscode-server.sh
@@ -1,5 +1,5 @@
  INSTALL_DIR=$(pwd)
- wget -O $INSTALL_DIR/openvscode-server.tar.gz https://github.com/gitpod-io/openvscode-server/releases/download/openvscode-server-v1.74.0/openvscode-server-v1.74.0-linux-arm64.tar.gz
+ wget -O $INSTALL_DIR/openvscode-server.tar.gz https://github.com/gitpod-io/openvscode-server/releases/download/openvscode-server-v1.74.0/openvscode-server-v1.74.0-linux-x64.tar.gz
  tar zxf $INSTALL_DIR/openvscode-server.tar.gz
  mv $INSTALL_DIR/openvscode-server-*/ $INSTALL_DIR/openvscode-server
  cd $INSTALL_DIR/openvscode-server
