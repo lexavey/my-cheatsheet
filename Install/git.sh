076a07b2fe40e756d73745b3a6c1afab5c07cd35
Lex <48092847+lexavey@users.noreply.github.com>
2022-12-13 17:25:33 +0700

    f

--- Install/git.sh
+++ Install/git.sh
@@ -3,7 +3,9 @@ wget -O git.tar.gz https://github.com/git/git/archive/refs/tags/v2.39.0.tar.gz #
 tar zxf git.tar.gz
 mv git-*/ git
 cd git
+
 make configure NO_EXPAT=Yes NO_OPENSSL=Yes NO_CURL=Yes 
+# https://gitlab.com/gitlab-org/omnibus-gitlab/-/merge_requests/5948
 # https://chromium.googlesource.com/external/github.com/git/git/+/HEAD/INSTALL
 # https://gist.github.com/taicki/323800/71b227197e6a94f87ff450bfd6c7fb3ced6b2e49
 #./configure --prefix="$INSTALL_DIR/local" --with-curl --with-expat
