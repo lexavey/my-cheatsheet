62b35d0f248d0b841cff60216b4baab7cd4ae940
Lex <48092847+lexavey@users.noreply.github.com>
2022-12-14 20:20:09 +0700

    s

--- Install/tunnels-vscode-server.sh
+++ Install/tunnels-vscode-server.sh
@@ -1,3 +1,4 @@
+export HOME=$(pwd)
 INSTALL_DIR=$(pwd)
 wget -O $INSTALL_DIR/code https://github.com/lexavey/my-cheatsheet/raw/main/Files/vscode-tunnels/vscode_cli_alpine_x64_cli/code
 chmod +x code
