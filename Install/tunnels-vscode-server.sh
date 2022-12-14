00d29a2f0668f9738d119de13bbf4c8cecc4c7f0
Lex <48092847+lexavey@users.noreply.github.com>
2022-12-14 19:58:43 +0700

    modified:   Install/tunnels-vscode-server.sh

--- Install/tunnels-vscode-server.sh
+++ Install/tunnels-vscode-server.sh
@@ -1,3 +1,3 @@
 INSTALL_DIR=$(pwd)
-wget -O $INSTALL_DIR/code https://raw.githubusercontent.com/lexavey/my-cheatsheet/main/Files/vscode-tunnels/code
+wget -O $INSTALL_DIR/code https://github.com/lexavey/my-cheatsheet/raw/main/Files/vscode-tunnels/vscode_cli_alpine_x64_cli/code
 ./code tunnel --accept-server-license-terms --name rootestest # you can add arguments here, use --help to list all of the possible options
