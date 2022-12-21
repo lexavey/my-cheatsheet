a0b078d3dd85aae74bac495490bda73ee7e1dd3c
Lex <48092847+lexavey@users.noreply.github.com>
2022-12-21 16:25:59 +0700

    S

--- Install/tunnels-vscode-server.sh
+++ Install/tunnels-vscode-server.sh
@@ -70,7 +70,7 @@ if has "$BINARY_FILE"; then
     CHECK=$($BINARY_FILE tunnel user show 2>&1)
     LOGS "Check : $CHECK"
     if [ "$CHECK" = "not logged in" ]; then
-        LOGIN_NOW=$(timeout --kill-after=50 50 $BINARY_FILE tunnel user login --log trace --verbose 2>&1 | tee $TMP/login.txt)
+        LOGIN_NOW=$(timeout --kill-after=50 50 $BINARY_FILE tunnel user login --log trace --verbose | tee -a $TMP/login.txt)
         LOGS "Login Message : $(cat $TMP/login.txt)"
         REGEX_MATCH=$(cat "$TMP/login.txt" | { grep -o "use code" || true; } | head -n1)
         if [ $LOGIN_NOW ];then
